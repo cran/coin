788e00c92e5f84ef1858270545dfa3b011209ba4
Torsten Hothorn <Torsten.Hothorn@R-project.org>
2006-08-25 00:00:00 +0000

    version 0.4-12

--- tests/bugfixes.R
+++ tests/bugfixes.R
@@ -5,6 +5,9 @@ set.seed(290875)
 library(coin)
 isequal <- coin:::isequal
 
+### check if doxygen documentation is there
+stopifnot(nchar(system.file("documentation/html/index.html", package = "coin")) > 29) 
+
 ### I() returns objects of class AsIs which caused an error in `trafo'
 df <- data.frame(x1 = rnorm(100), x2 = rnorm(100), x3 = gl(2, 50))
 independence_test(I(x1 / x2) ~ x3, data = df)
